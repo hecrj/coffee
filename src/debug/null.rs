6dc1d831c2e87a4dc9c9e58aeab266450e4a6b1c
Héctor Ramón Jiménez <hector0193@gmail.com>
2019-04-25 18:39:04 +0200

    Fix `Debug` failing to compile on release mode

--- src/debug/null.rs
+++ src/debug/null.rs
@@ -2,11 +2,13 @@ use crate::graphics;
 
 // Null debug implementation
 #[cfg(not(debug_assertions))]
+#[allow(missing_debug_implementations)]
+#[allow(missing_docs)]
 pub struct Debug {}
 
 #[cfg(not(debug_assertions))]
 impl Debug {
-    pub fn new(_gpu: &mut graphics::Gpu, _draw_rate: u16) -> Self {
+    pub(crate) fn new(_gpu: &mut graphics::Gpu, _draw_rate: u16) -> Self {
         Self {}
     }
 
@@ -17,9 +19,9 @@ impl Debug {
     pub(crate) fn frame_started(&mut self) {}
     pub(crate) fn frame_finished(&mut self) {}
 
-    pub(crate) fn event_loop_started(&mut self) {}
+    pub(crate) fn interact_started(&mut self) {}
 
-    pub(crate) fn event_loop_finished(&mut self) {}
+    pub(crate) fn interact_finished(&mut self) {}
 
     pub(crate) fn update_started(&mut self) {}
 
@@ -35,5 +37,6 @@ impl Debug {
         false
     }
 
+    #[allow(missing_docs)]
     pub fn draw(&mut self, _frame: &mut graphics::Frame) {}
 }
