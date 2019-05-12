1fd6eb5c7a3ce1dc845b7cc4096a7207c59f43fe
Héctor Ramón Jiménez <hector0193@gmail.com>
2019-05-12 19:50:30 +0200

    Check selected graphics backend in `build.rs`

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -96,17 +96,6 @@
 //! [`Font`]: struct.Font.html
 //! [`Game::draw`]: ../trait.Game.html#tymethod.draw
 //! [`Game::new`]: ../trait.Game.html#tymethod.new
-#[cfg(not(any(
-    feature = "opengl",
-    feature = "vulkan",
-    feature = "metal",
-    feature = "dx11",
-    feature = "dx12"
-)))]
-compile_error!(
-    "You need to enable a graphics backend feature. \
-     Available options: opengl, vulkan, metal, dx11, dx12."
-);
 
 #[cfg(feature = "opengl")]
 mod backend_gfx;
