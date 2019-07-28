520acb7956f748cd695e8f36c19c532d5e9bceb8
Héctor Ramón Jiménez <hector0193@gmail.com>
2019-07-29 01:19:18 +0200

    Use `Matrix3` instead of `Matrix4` in `Transformation`

--- src/graphics/transformation.rs
+++ src/graphics/transformation.rs
@@ -1,8 +1,7 @@
-use nalgebra::{Matrix4, Point3, Vector3};
+use nalgebra::Matrix3;
 use std::ops::Mul;
 
-use crate::graphics::point::Point;
-use crate::graphics::vector::Vector;
+use crate::graphics::Vector;
 
 /// A 2D transformation matrix.
 ///
@@ -10,12 +9,12 @@ use crate::graphics::vector::Vector;
 ///
 /// [`Target`]: struct.Target.html
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Transformation(Matrix4<f32>);
+pub struct Transformation(Matrix3<f32>);
 
 impl Transformation {
     /// Get the identity transformation.
     pub fn identity() -> Transformation {
-        Transformation(Matrix4::identity())
+        Transformation(Matrix3::identity())
     }
 
     /// Creates an orthographic projection.
@@ -24,9 +23,12 @@ impl Transformation {
     /// set up with the correct orthographic projection.
     ///
     /// [`Target`]: struct.Target.html
+    #[rustfmt::skip]
     pub fn orthographic(width: f32, height: f32) -> Transformation {
-        Transformation(nalgebra::Matrix4::new_orthographic(
-            0.0, width, 0.0, height, -1.0, 1.0,
+        Transformation(nalgebra::Matrix3::new(
+            2.0 / width, 0.0, -1.0,
+            0.0, 2.0 / height, -1.0,
+            0.0, 0.0, 1.0
         ))
     }
 
@@ -34,10 +36,9 @@ impl Transformation {
     ///
     /// You can use this to pan your camera, for example.
     pub fn translate(translation: Vector) -> Transformation {
-        Transformation(Matrix4::new_translation(&Vector3::new(
+        Transformation(Matrix3::new_translation(&Vector::new(
             translation.x,
             translation.y,
-            0.0,
         )))
     }
 
@@ -45,7 +46,7 @@ impl Transformation {
     ///
     /// You can use this to zoom your camera, for example.
     pub fn scale(scale: f32) -> Transformation {
-        Transformation(Matrix4::new_scaling(scale))
+        Transformation(Matrix3::new_scaling(scale))
     }
 
     /// Creates a non-uniform scale transformation.
@@ -53,16 +54,14 @@ impl Transformation {
     /// It allows you to scale each axis independently. You should rarely need
     /// this.
     pub fn nonuniform_scale(scale: Vector) -> Transformation {
-        Transformation(Matrix4::new_nonuniform_scaling(&Vector3::new(
-            scale.x, scale.y, 1.0,
-        )))
+        Transformation(Matrix3::new_nonuniform_scaling(&scale))
     }
 
     /// Creates a rotation transformation (in radians).
     ///
     /// You can use this to rotate your camera, for example.
     pub fn rotate(rotation: f32) -> Transformation {
-        Transformation(Matrix4::new_rotation(Vector3::new(0.0, 0.0, rotation)))
+        Transformation(Matrix3::new_rotation(rotation))
     }
 }
 
@@ -75,29 +74,37 @@ impl Mul for Transformation {
 }
 
 impl From<Transformation> for [[f32; 4]; 4] {
-    fn from(transformation: Transformation) -> [[f32; 4]; 4] {
-        transformation.0.into()
-    }
-}
-
-impl From<Transformation> for [f32; 16] {
-    fn from(t: Transformation) -> [f32; 16] {
+    #[rustfmt::skip]
+    fn from(t: Transformation) -> [[f32; 4]; 4] {
         [
-            t.0[0], t.0[1], t.0[2], t.0[3], t.0[4], t.0[5], t.0[6], t.0[7],
-            t.0[8], t.0[9], t.0[10], t.0[11], t.0[12], t.0[13], t.0[14],
-            t.0[15],
+            [t.0[0], t.0[1], 0.0, t.0[2]],
+            [t.0[3], t.0[4], 0.0, t.0[5]],
+            [0.0, 0.0, -1.0, 0.0],
+            [t.0[6], t.0[7], 0.0, t.0[8]],
         ]
     }
 }
 
-impl From<Matrix4<f32>> for Transformation {
-    fn from(matrix: Matrix4<f32>) -> Self {
+impl From<Transformation> for [f32; 16] {
+    #[rustfmt::skip]
+    fn from(t: Transformation) -> [f32; 16] {
+        [
+            t.0[0], t.0[1], 0.0, t.0[2],
+            t.0[3], t.0[4], 0.0, t.0[5],
+            0.0, 0.0, -1.0, 0.0,
+            t.0[6], t.0[7], 0.0, t.0[8]
+        ]
+    }
+}
+
+impl From<Matrix3<f32>> for Transformation {
+    fn from(matrix: Matrix3<f32>) -> Self {
         Transformation(matrix)
     }
 }
 
-impl Into<Matrix4<f32>> for Transformation {
-    fn into(self) -> Matrix4<f32> {
+impl Into<Matrix3<f32>> for Transformation {
+    fn into(self) -> Matrix3<f32> {
         self.0
     }
 }
